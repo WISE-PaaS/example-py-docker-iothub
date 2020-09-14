4840b30a2e76b16812e00fc0142c48c5d9a80ed2
KChen <45040671+tecd0721@users.noreply.github.com>
2020-09-14 14:13:07 +0800

    Update publisher.py

--- publisher.py
+++ publisher.py
@@ -1,16 +1,23 @@
 import paho.mqtt.client as mqtt
 import random
 
+# mqtt credentials in RabbitMQ
 broker="rabbitmq-001-pub.hz.wise-paas.com.cn"
 mqtt_port=1883
 username="d34d32b7-5256-4c48-90b6-1cdc3cf2bd35:40741fb9-44e8-48ee-bf73-50cc1c6dcfea"
 password="J9DeTWe30rizaK7lR9Ds"
 
-def on_publish(client,userdata,result):             #create function for callback
+# create function for callback
+def on_publish(client,userdata,result):
     print("data published")
 
-client= mqtt.Client()                           #create client object
+# create client object
+client= mqtt.Client()       
+# Set login account password
 client.username_pw_set(username,password)
-client.on_publish = on_publish                          #assign function to callback
-client.connect(broker,mqtt_port)                                 #establish connection
+# assign function to callback
+client.on_publish = on_publish               
+# establish connection
+client.connect(broker,mqtt_port)          
+# Published topics and content
 client.publish("/hello",random.randint(10,30))
